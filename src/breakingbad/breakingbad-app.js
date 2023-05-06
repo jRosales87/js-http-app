c3161e267008243c5d294f054d66bb85162d5baa
jrosales2022 <juanrosales@ayudat.es>
2023-05-06 21:30:35 +0200

    feat: añadir archivos iniciales del proyecto feat: añadir estilos básicos al proyecto feat: añadir funcionalidad para mostrar citas de Breaking Bad feat: añadir dependencia de desarrollo vite al proyecto feat: añadir archivo .gitignore al proyecto feat: añadir archivo package.json al proyecto feat: añadir archivo index.html al proyecto feat: añadir archivo javascript.svg al proyecto feat: añadir archivo vite.svg al proyecto

---
+++ src/breakingbad/breakingbad-app.js
@@ -0,0 +1,49 @@
+
+/**
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async() => {
+
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    
+    const data = await res.json();
+    
+    return data[0];
+}
+
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingbadApp = async(element) => {
+
+    document.querySelector('#app-title').innerHTML = "Breaking Bad App";
+    element.innerHTML = 'Loading...';
+
+    // await fetchQuote();
+
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+
+    nextQuoteButton.innerText = 'Next Quote';
+    
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren( quoteLabel, authorLabel, nextQuoteButton);
+    }
+    
+    
+    await fetchQuote()
+    .then( renderQuote );
+    
+    nextQuoteButton.addEventListener('click', async() => {
+        element.innerHTML = 'Loading...';
+        await fetchQuote()
+            .then( renderQuote );
+    });
+}
